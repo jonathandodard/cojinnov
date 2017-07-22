2033db24a536b429c7f0af6db70cdf7ddf8d6498
jonathandodard <jonathan.dodard@gmail.com>
2017-07-22 17:00:18 +0200

    crud product and

---
+++ src/AppBundle/Controller/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonathan
+ * Date: 22/07/17
+ * Time: 14:24
+ */
+
+namespace AppBundle\Controller;
+
+
+class ProductController
+{
+
+}
---
+++ src/AppBundle/Form/ProductType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonathan
+ * Date: 22/07/17
+ * Time: 14:32
+ */
+
+namespace AppBundle\Form;
+
+
+class ProductType
+{
+
+}
