a43f9e6e2c068ec243fec748d48644d2e428a4b0
jonathan <j.dodard@krealinks.com>
2017-09-12 12:26:06 +0200

    update app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,9 +10,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+$allowedIps = array(
+    '82.120.29.42',
+);
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
+    || (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !in_array($_SERVER['HTTP_X_FORWARDED_FOR'], $allowedIps))
+
+//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+//    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
--- web/js/mains.js
+++ web/js/mains.js
@@ -1,5 +1,26 @@
 $('.button-collapse').sideNav({
         menuWidth: 300, // Default is 300
         edge: 'right',
-    }
-  );
+});
+
+// $('.dateAt').on('click',function () {
+//     $(this).pickadate({
+//         selectMonths: true, // Creates a dropdown to control month
+//         selectYears: 15, // Creates a dropdown of 15 years to control year,
+//         today: 'Today',
+//         clear: 'Clear',
+//         close: 'Ok',
+//         closeOnSelect: false // Close upon selecting a date,
+//     });
+// });
+// $('.dateTo').on('click',function () {
+//     $(this).pickadate({
+//         selectMonths: true, // Creates a dropdown to control month
+//         selectYears: 15, // Creates a dropdown of 15 years to control year,
+//         format: 'd mmmm, yyyy',
+//         today: 'Today',
+//         clear: 'Clear',
+//         close: 'Ok',
+//         closeOnSelect: false // Close upon selecting a date,
+//     });
+// });
